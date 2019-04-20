2d4ae14755088ede4e21d5438d84441a9839cb9a
hasparus <hasparus@gmail.com>
2019-04-20 12:56:55 +0200

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-[![Edit playing-around-react-three-fiber](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/github/hasparus/hasparus.github.io/tree/master/?fontsize=14)
---
+++ precache-manifest.08ec6d5b8adf59008fc64b6d06f5b7af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd321f8f0688075abf7f",
+    "url": "/static/css/main.a961ca61.chunk.css"
+  },
+  {
+    "revision": "bd321f8f0688075abf7f",
+    "url": "/static/js/main.bd321f8f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "527099a74a16d9269be6",
+    "url": "/static/js/2.527099a7.chunk.js"
+  },
+  {
+    "revision": "265a7a4992cb4e35c2978ae9f0da8231",
+    "url": "/index.html"
+  }
+];
